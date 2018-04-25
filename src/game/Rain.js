7be082fae3a5dcf2fb083fabcaed92287d6d76af
Matthias Piepkorn <mpiepk@gmail.com>
2018-04-25 18:22:05 +0200

    further tweak rain

--- src/game/Rain.js
+++ src/game/Rain.js
@@ -2,9 +2,9 @@ import store from "../store";
 import {writeLog} from "../actions/log";
 import {raining, wipers} from "../actions/game";
 
-export default class Radio {
+export default class Rain {
   update(state) {
-    if (!state.raining && Math.random() > 0.9999 && state.startTime) {
+    if (!state.raining && Math.random() > 0.9995 && state.startTime && state.time - state.startTime > 45) {
       setTimeout(() => store.dispatch(raining(true)));
       setTimeout(() => store.dispatch(writeLog('It started raining. Bruh. What\'s next? Grannys on the road?')));
     } else if (state.raining && Math.random() > 0.997) {
